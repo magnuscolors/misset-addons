34feff7831d39f090253fa28022299b790a97eb6
Stephan van de Berg <berg.stephan@gmail.com>
2019-04-17 16:58:49 +0200

    update 2

--- misset_expenses/__init__.py
+++ misset_expenses/__init__.py
@@ -20,6 +20,4 @@
 #
 ##############################################################################
 
-import models
-import report
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
