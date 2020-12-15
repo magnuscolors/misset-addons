d9d27780c9b5eccdc2418b27f94cbc92dbe30ae1
Sify K Raphy <sifykr@systemsvalley.co.in>
2020-12-15 17:19:28 +0530

    commented the model

--- misset_maintenance/models/models.py
+++ misset_maintenance/models/models.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
-class misset_maintenance(models.Model):
-    _name = 'misset_maintenance.misset_maintenance'
-
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
+# class misset_maintenance(models.Model):
+#     _name = 'misset_maintenance.misset_maintenance'
+#
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
